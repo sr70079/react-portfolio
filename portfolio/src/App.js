5011bb538adafda94f78356efe24d918239e7286
Sarah Marshall <sr70079@gmail.com>
2021-05-13 21:36:50 -0400

    file structure setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter as Router , Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+<Router>
+  <Route exact path = "/" component = {Home}/>
+  <Route exact path = "/projects" component = {Projects}/>
+  <Route exact path = "/contact" component = {Contact}/>
+  </Router>
+<Footer />
+    </div>
+  );
+}
+
+export default App;
